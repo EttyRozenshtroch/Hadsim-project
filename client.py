7b14e4650220c8c294ef7d256a19e256dc7c7056
Etty <Etty4423@gmail.com>
2023-05-15 12:36:48 +0300

    last update

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ person=['ari','segal','987654321','jerusalem','yafo',26,'1990-1-21','025326569',
 
 
 def get_data_from_server():
-    url = 'http://127.0.0.1:5000/Employees/123456789'  # ה-URL של השרת שברצונך לשלוח לו את הבקשה
+    url = 'http://127.0.0.1:5000/Employees/123456789/CoronaDetales'  # ה-URL של השרת שברצונך לשלוח לו את הבקשה
     response = requests.get(url)
     if response.status_code == 200:  # בדיקת קוד המצב של התגובה - 200 משמעו שהבקשה בוצעה בהצלחה
         data = response.json()  # קריאת הנתונים מתוך התגובה
@@ -12,14 +12,17 @@ def get_data_from_server():
         print(f"Request failed with status code: {response.status_code}")
 
 def put_into():
-    url = 'http://127.0.0.1:5000/Employees'
-    data={'first_name': 'first_name_1', 'last_name': 'last_name_1', 'customerID': 1111, 'city': 'city_1', 'street': 'street_1', 'house_number': 1, 'phone': '11', 'mobile_phone': '1111'}
-    response = requests.post(url, json=person,headers = {'Content-Type': 'application/json'})
+    # url = "http://127.0.0.1:5000/Employees/123456789/CoronaDetales/addVaccina?vaccina_code=222&vaccina_date='2021-12-30'"
+    url = "http://127.0.0.1:5000/Employees/987654321/CoronaDetales/add_positive_result?date=2022-01-01"
+    # data={'first_name': 'first_name_1', 'last_name': 'last_name_1', 'customerID': 1111, 'city': 'city_1', 'street': 'street_1', 'house_number': 1, 'phone': '11', 'mobile_phone': '1111'}
+    # response = requests.post(url, json=person,headers = {'Content-Type': 'application/json'})
+    response = requests.post(url)
     return response
 
-# ppppp=put_into()
-# print(ppppp)
+# קריאה לפונקצית עדכון והדפסת סטטוס הצלחה/כישלון
+try_put=put_into()
+print(try_put)
 
 # קריאה לפונקציה והדפסת הנתונים שהתקבלו
-data = get_data_from_server()
-print(data)
+# data = get_data_from_server()
+# print(data)
--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 import pyodbc as pyodbc
 from flask import Flask, Response, request ,make_response, jsonify
+import json
+
 conn_str = "DRIVER={SQL Server};SERVER=DESKTOP-HI24610\SQLEXPRESS;DATABASE=HealthClinicDB;"
 app = Flask(__name__)
 
 select_all_query="SELECT *FROM Employees JOIN CoronaDetails ON CoronaDetails.IDNumber = Employees.IDNumber JOIN People_Vaccinations ON People_Vaccinations.EmployeeID = Employees.IDNumber JOIN Vaccines ON Vaccines.VaccineID = People_Vaccinations.VaccinationID;"
 select_employys_query="SELECT *FROM Employees"
-
-def fetch_data_from_sql(query): #שליפת נתונים מSQL
+select_employys_by_id_query="SELECT * FROM Employees WHERE IDNumber = ?"
+select_corona_detals_by_id="SELECT *FROM Employees JOIN CoronaDetails ON CoronaDetails.IDNumber = Employees.IDNumber JOIN People_Vaccinations ON People_Vaccinations.EmployeeID = Employees.IDNumber JOIN Vaccines ON Vaccines.VaccineID = People_Vaccinations.VaccinationID WHERE  CoronaDetails.IDNumber=?"
+def fetch_data_from_sql(query):    #שליפת נתונים מSQL
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
     try:
-        cursor.execute(query)
-        columns = [column[0] for column in cursor.description]
+        cursor.execute(query)   # הרצת השאילתה
+        columns = [column[0] for column in cursor.description]  # הגדרת העמודות
         rows_list = []
         for row in cursor.fetchall():
             row_dict = dict(zip(columns, row))
@@ -28,14 +31,12 @@ def fetch_column_from_sql(query,ID):
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
     try:
-        cursor.execute(query, ID)
-        results = cursor.fetchall()
-        aaaa=[]
-        for row in results:
-            ########
-            print(row)
-        cursor.execute(query)
-
+        cursor.execute(query,ID)
+        row_data=cursor.fetchone()
+        columns = [column[0] for column in cursor.description]
+        row_dict = dict(zip(columns, row_data))
+        json_data = json.dumps(row_dict)
+        return json_data
     except:
         return False
     finally:
@@ -52,7 +53,7 @@ def get_employees():
     response = make_response(jsonify(rows_list))
     response.status_code = 200
     return response
-@app.route('/Employees', methods=['POST'])
+@app.route('/Employees', methods=['POST'])# הכנסת נתונים לבנ"א
 def insert_employee_into_DB():
     person = request.get_json()
     query=f"INSERT INTO[dbo].[Employees] VALUES('{person[0]}', '{person[1]}', '{person[2]}', '{person[3]}', '{person[4]}', {person[5]}, '{person[6]}', '{person[7]}', '{person[8]}')"
@@ -67,21 +68,19 @@ def insert_employee_into_DB():
     except:
         response.status_code=500
     finally:
+        cursor.close()
         conn.close()
         return response
 
-@app.route('/Employees/123456789', methods=['GET'])
-def get_employees_by_ID():
-    # query=f"select * from Employees WHERE IDNumber =123456789"
-    # print(f{employy_id})
+@app.route('/Employees/<emp_id>', methods=['GET'])# שליפת נתונים לאדם ע"פ ת"ז
+def get_employees_by_ID(emp_id):
     query="SELECT * FROM Employees WHERE IDNumber = ?"
-    id="123456789"
-    data=fetch_column_from_sql(query,id)
+    data=fetch_column_from_sql(query,emp_id)
     response = make_response(jsonify(data))
     response.status_code = 200
     return response
 
-@app.route('/Employees/CoronaDetales', methods=['GET'])
+@app.route('/Employees/CoronaDetales', methods=['GET'])# שליפת טבלת האנשים עם החיסונים שלהם
 def get_employees_and_corona_details():
     data=fetch_data_from_sql(select_all_query)
     rows_list = []
@@ -92,31 +91,86 @@ def get_employees_and_corona_details():
     return response
 
 
-def insert_data_into_database(data):
-    conn = pyodbc.connect(conn_str)
-    # יצירת מופע של סוגר קורא
-    cursor = conn.cursor()
+@app.route('/Employees/<emp_id>/CoronaDetales', methods=['GET']) #קבלת נתוני קורונה לאדם ע"פ ת"ז
+def get_corona_detales_by_id(emp_id):
+    query=f"SELECT *FROM Employees JOIN CoronaDetails ON CoronaDetails.IDNumber = Employees.IDNumber JOIN People_Vaccinations ON People_Vaccinations.EmployeeID = Employees.IDNumber JOIN Vaccines ON Vaccines.VaccineID = People_Vaccinations.VaccinationID WHERE  CoronaDetails.IDNumber={emp_id}"
+    conn=pyodbc.connect(conn_str)
     try:
         with conn.cursor() as cursor:
-            # הכנת השאילתה עם הנתונים המתקבלים
-            sql = "INSERT INTO table_name (column1, column2) VALUES (?, ?)"
-            cursor.execute(sql, (data['column1'], data['column2']))
-        conn.commit()
-        return True
+            cursor.execute(query)
+            columns = [column[0] for column in cursor.description]
+            rows_list = []
+            data=cursor.fetchall()
+            print(data)
+            for row in data:
+                row_dict = dict(zip(columns, row))
+                rows_list.append(row_dict)
+                print(row_dict)
+        response = make_response(jsonify(rows_list),200)
     except:
-        return False
+        response=make_response()
+        response.status_code(500)
     finally:
+        cursor.close()
+        conn.close()
+        return response
+
+@app.route('/Employees/<emp_id>/CoronaDetales/addVaccina', methods=['POST'])  #הוספת חיסון לעובד
+def add_vaccina_to_employee(emp_id):
+    vaccina_code= request.args.get('vaccina_code')
+    vaccina_date= request.args.get('vaccina_date')
+    query=f"SELECT COUNT(VaccinationID) FROM dbo.People_Vaccinations WHERE EmployeeID = '{emp_id}';"
+    insert_query = f"Insert into People_Vaccinations VALUES('{emp_id}',{vaccina_code},{vaccina_date})"
+    conn = pyodbc.connect(conn_str)
+    response = make_response()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            result=cursor.fetchone()
+            number_of_rows=int(result[0])
+    except:
+        print('error')
+        response.status_code(500)
+    finally:
+        cursor.close()
         conn.close()
 
-@app.route('/data',methods=['post'])
-def insert_handler():
-    data = request.get_json()
-    print(data)
-    success = insert_data_into_database(data)
-    if success:
-        return 'הנתונים הוזנו בהצלחה לבסיס הנתונים'
+    if number_of_rows >=4:
+        response.status_code (500)
+        return response
     else:
-        return 'שגיאה בהכנסת הנתונים לבסיס הנתונים'
+        try:
+            conn = pyodbc.connect(conn_str)
+            with conn.cursor() as cursor:
+                cursor.execute(insert_query)
+            conn.commit()
+            response.status_code ( 200)
+        except:
+            response.status_code  (500)
+        finally:
+            cursor.close()
+            conn.close()
+            return response
+
+@app.route('/Employees/<emp_id>/CoronaDetales/add_positive_result', methods=['POST'])  #הוספת תאריך מחלה לאדם
+def add_positive_result(emp_id):
+    date_positive_result = request.args.get('date')
+    insert_query=f"Insert into CoronaDetails (IDNumber,DataPositiveResult) VALUES('{emp_id}','{date_positive_result}')"
+    conn = pyodbc.connect(conn_str)
+    response = make_response()
+    try:
+        conn = pyodbc.connect(conn_str)
+        with conn.cursor() as cursor:
+            cursor.execute(insert_query)
+        conn.commit()
+        response.status_code ( 200)
+    except:
+        response.status_code  (500)
+    finally:
+        cursor.close()
+        conn.close()
+        return response
+
 
 
 port_number=5000
